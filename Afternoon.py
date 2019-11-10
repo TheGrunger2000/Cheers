f304fd3e0c6f53fcab20e33a6a75d560e6ac6d39
TheGrunger2000 <grungerdiary@gmail.com>
2019-11-10 19:56:42 +0300

    10.11.2019: Infinite cycle

--- Afternoon.py
+++ Afternoon.py
@@ -3,3 +3,17 @@ print("Afternoon, everybody!")
 print("Hey Norm, What's up?")
 
 print("It's a dog eat dog world, and I'm wearing MilkBone underwear")
+
+while True:
+    print("Enter the number")
+    choose = int(input(">>"))
+
+    if choose == 1:
+        print("OK, that's great!")
+        break
+    elif choose == 2:
+        print("Go and find a MIlkBone Box!")
+        break
+    else:
+        print("Well...")
+        break
